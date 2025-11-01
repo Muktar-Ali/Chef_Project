afc50cdd55ef0320a7de4e2d0a0d4b8c72222aa7
Muktar Ali <muktarali1004@gmail.com>
2025-11-01 15:15:11 -0400

    Refactored code and added recipe placeholder

--- chef-clay/src/Body.jsx
+++ chef-clay/src/Body.jsx
@@ -1,13 +1,35 @@
+import React from "react";
+import ClayRecipe from "./components/ClayRecipe";
+import IngredientsList from "./components/IngredientsList";
 
-export default function Body(){
-    return(
-        <main>
-            <form className="ingredient_form">
-                <input 
-                type="text"
-                placeholder="e.g. Flour"/>
-                <button>Add ingredient</button>
-            </form>
-        </main>
-    )
+export default function Body() {
+  const [ingredients, setIngredients] = React.useState([]);
+
+  const [recipeShown, setRecipeShown] = React.useState(false);
+
+ 
+
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredient) => [...prevIngredient, newIngredient]);
+  }
+
+  function toggleRecipeShown(){
+        setRecipeShown(prevShown => !prevShown)
+  }
+
+  return (
+    <main>
+      <form action={addIngredient} className="ingredient_form">
+        <input type="text" name="ingredient" placeholder="e.g. Flour" />
+
+        <button>Add ingredient</button>
+      </form>
+      {ingredients.length > 0 && <IngredientsList
+      ingredients = {ingredients}
+      toggleRecipeShown = {toggleRecipeShown}
+      /> }
+      {recipeShown && <ClayRecipe />}
+    </main>
+  );
 }
---
+++ chef-clay/src/components/IngredientsList.jsx
@@ -0,0 +1,23 @@
+export default function IngredientsList(props){
+     const ingredientList = props.ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ))
+
+    return(
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="listIngredients">
+            <li>{ingredientList}</li>
+          </ul>
+          {props.ingredients.length > 3 && (
+            <div className="get-recipe-container">
+              <div>
+                <h3>Ready for a recipe?</h3>
+                <p>Generate a recipe from your list of ingredients.</p>
+              </div>
+              <button onClick={props.toggleRecipeShown}>Get a recipe</button>
+            </div>
+          )}
+        </section>
+    )
+}
